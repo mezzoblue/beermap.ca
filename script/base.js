83353ac12c6b83bda6191095abc503ec148b1d01
Dave Shea <dave@mezzoblue.com>
2014-07-15 00:05:11 -0700

    Okay, getting somewhere with this

--- script/base.js
+++ script/base.js
@@ -20,8 +20,28 @@
 // setView: [lat, long], zoom level 
 var map = L.mapbox.map('map', 'mezzoblue.map-0311vf6d').setView([46, -107.215], 4);
 
-// load each data file into its own marker layer
-var mainLayer = L.markerClusterGroup();
+
+
+
+// -------------------------------------
+// clustergroup playground
+
+// loading markers as ClusterGroups
+var mainLayer = L.markerClusterGroup({
+
+    maxClusterRadius: 20,
+    showCoverageOnHover: false,
+
+    iconCreateFunction: function(cluster) {
+        return new L.DivIcon({ html: 
+
+            '<div>' + cluster.getChildCount() + '</div>'
+
+        });
+        addMarkers(cluster);
+    }
+
+});
 
 function readJSON(file) {
     var request = new XMLHttpRequest();
@@ -31,15 +51,78 @@ function readJSON(file) {
         return request.responseText;
 };
 
+// need to turn this into an init function
 var myObject = JSON.parse(readJSON('../canadian-craft-breweries/canadian-craft-breweries.geojson'));
-var geoJsonLayer = L.geoJson(myObject);
- mainLayer.addLayer(geoJsonLayer);
- map.addLayer(mainLayer);
 
-// mainLayer.loadURL('../canadian-craft-breweries/canadian-craft-breweries.geojson');
+var geoJsonLayer = L.geoJson(myObject, {
+
+    onEachFeature: function(feature, layer) {
+
+        // Create custom popup content
+        // (this is super ugly)
+        var popupContent =  '<div class="map-popup">';
+
+        // title of brewery
+        if (feature.properties.URL) {
+            popupContent += '<h2><a class="main-link" href="' + feature.properties.URL  + '">' + feature.properties.Name + '</a></h2>';
+        } else {
+            popupContent += '<h2>' + feature.properties.Name + '</h2>';
+        }
+
+
+        popupContent +=     '   <ul>';
+
+        if (feature.properties.AKA) {
+            popupContent +=     '       <li class="aka">aka ' + feature.properties.AKA + '</li>';
+        }
+
+        popupContent +=     '       <li>' + feature.properties.City + ', ' + feature.properties.Province + '</li>';
+
+
+        // conditional extra information
+
+        if (feature.properties.Note) {
+            popupContent +=     '      <li class="note">' + feature.properties.Note  + '</li>';
+        }
+
+        popupContent +=     '      <li class="extra">';
+
+        if (feature.properties.Twitter) {
+            popupContent += '<a class="twitter" href="' + feature.properties.Twitter  + '">T</a>';
+        }
+        if (feature.properties.Facebook) {
+            popupContent += '<a class="fb" href="' + feature.properties.Facebook  + '">F</a>';
+        }
+        if ((feature.properties.Type) && (feature.properties.Type != "Unknown")) {
+            popupContent += '<span class="type">' + feature.properties.Type + '</span>';
+        }
+
+        popupContent +=     '      </li>';
+
+        if (feature.properties.Closed) {
+            popupContent +=     '      <li class="closed">Closed: ' + feature.properties.Closed  + '</li>';
+        }
+
+        popupContent +=     '   </ul>';
+        popupContent +=     '</div>';
+
+
+        // whew, let's add it.
+        layer.bindPopup(popupContent);
+
+    }
+
+});
+
+mainLayer.addLayer(geoJsonLayer);
+map.addLayer(mainLayer);
+
+
+// -------------------------------------
 
 
 
+// load each data file into its own marker layer
 var rumourLayer = L.mapbox.markerLayer()
     .loadURL('../canadian-craft-breweries/upcoming-rumoured.geojson');
 
@@ -244,9 +327,6 @@ var mapIcons = setMediumIcons();
 
 // Add custom markers & popups to each using our custom feature properties
 
-mainLayer.on('layeradd', function(e) {
-	addMarkers(e);
-});
 rumourLayer.on('layeradd', function(e) {
 	addMarkers(e);
 });
